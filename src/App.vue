c25974e9c9a8a214e53fb18f8119c846ea6c05e5
2685658468 <javascript.h@qq.com>
2019-01-04 09:55:24 +0800

    入口文件

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
+    <!--进入离开歌曲详情播放页时控制导航显示(有过渡) -->
+    <transition name="enterSongDetail" modth="out-in">
     <!-- :gutter="20" 间隔 -->
-    <el-row class="header_nav" :gutter="0">
+    <el-row id="header_nav" :gutter="0" v-show="showNav">
       <el-col :span="8" class="header_nav_item">
         <router-link to="/" exact @click.native="fun_app($event)" tag="div">推荐音乐</router-link>
       </el-col>
@@ -12,6 +14,7 @@
         <router-link to="/search" @click.native="fun_app($event)" tag="div">搜索</router-link>
       </el-col>
     </el-row>
+    </transition>
     <!-- modth = 'out-in' 动画时间顺序 -->
     <!-- <router-view>外层加div 设置为相对定位,内部视图为绝对定位,
     既可以撑开html,路由切换动画也不会因为元素脱离文档流而瞬间上下抖动-->
@@ -20,26 +23,46 @@
     <transition name="changePage" modth="out-in">
       <!-- exclude = 不缓存的路由 -->
       <keep-alive exclude="SongSheet">
-        <router-view style="position:absolute;top:0.43rem"></router-view>
+        <router-view style=""></router-view>
       </keep-alive>
     </transition>
+     <Myaudio :src="songSrcPic.src" @loaded="loaded"></Myaudio>
     <!-- </div> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Myaudio from "@/components/musicPlayer.vue";
 export default {
   name: "App",
+  // 修改data中的值会触发绑定的视图更新,但修改data依赖的值,不会触发绑定的视图更新,data只初始化一次
+  data() {
+    return {
+   
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Myaudio
   },
   created() {
     
   },
+  computed: {
+      showNav:function(){
+         return this.$store.state.showNav
+      },
+      songSrcPic:function(){
+         return this.$store.state.songSrcPic 
+      }
+  },
   methods: {
     fun_app(e) {
       // console.log(e.target.innerHTML);
+    },
+    loaded(param){
+      console.log(param);
     }
   }
 };
@@ -71,21 +94,24 @@ body::-webkit-scrollbar {
   text-align: center;
   color: #2c3e50;
   background: #eee;
-  margin-top: 20px;
   font-size: 16px;
 }
-.header_nav {
+#header_nav {
+  position: absolute;
+  width: 100%;
+  height: 0.45rem;
   border-bottom: 1px solid #ddd;
   padding: 5px 6px 0px 6px;
   font-size: 16px;
   color: black;
+  margin-top: 20px;
 }
 .router-link-active {
   color: #d33a31 !important;
   text-align: center;
   box-sizing: content-box;
 }
-.router-link-active::after {
+.router-link-active::after{
   content: "";
   display: block;
   border-bottom: 2px solid #d33a31;
@@ -146,6 +172,31 @@ body::-webkit-scrollbar {
   transform: translateX(-150px);
 }
 .changePage-leave-active {
+  transition: 0.4s ease;
+}
+/* 导航 v-show 动画 */
+.enterSongDetail{
+  backface-visibility: hidden;
+  perspective: 1000;
+  transform: translate3d(0, 0, 0);
+}
+.enterSongDetail-enter {
+  transform: translateY(-150px);
+}
+.enterSongDetail-enter-to {
+  transform: translateY(0px);
+}
+
+.enterSongDetail-enter-active {
+  transition: 0.6s ease ;
+}
+.enterSongDetail-leave {
+  transform: translateY(0px);
+}
+.enterSongDetail-leave-to {
+  transform: translateY(-150px);
+}
+.enterSongDetail-leave-active {
   transition: 0.4s ease;
 }
 </style>
